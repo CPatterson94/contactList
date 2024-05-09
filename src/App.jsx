ebb4fc9fb35fcb8156f724dc2692d27bafd2e518
Curtis Patterson <Dpatter94@yahoo.com>
2024-05-09 12:18:37 -0400

    Created Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./index.css";
+
+function App() {
+  const [,] = useState(0);
+
+  return (
+    <>
+      <div className="wrapper"></div>
+    </>
+  );
+}
+
+export default App;
